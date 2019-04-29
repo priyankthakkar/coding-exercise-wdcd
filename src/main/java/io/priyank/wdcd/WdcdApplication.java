d8d523ffeb09af649ea97e64db74cb0bdcea2eff
Priyank Thakkar <priyankthakkar@gmail.com>
2019-04-30 08:06:44 +1000

    Change Log: 1. Initial commit Changes By: Priyank Thakkar

---
+++ src/main/java/io/priyank/wdcd/WdcdApplication.java
@@ -0,0 +1,13 @@
+package io.priyank.wdcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WdcdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WdcdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/priyank/wdcd/WdcdApplicationTests.java
@@ -0,0 +1,16 @@
+package io.priyank.wdcd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WdcdApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
