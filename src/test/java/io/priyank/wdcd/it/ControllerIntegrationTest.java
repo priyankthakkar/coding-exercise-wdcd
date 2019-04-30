f8d2de3aa37dcf50d2b93ff40d0ffa1734823372
Priyank Thakkar <priyankthakkar@gmail.com>
2019-04-30 16:15:26 +1000

    Change Log: 1. Cleaning unwanted imports Changes By: Priyank Thakkar

--- src/test/java/io/priyank/wdcd/it/ControllerIntegrationTest.java
+++ src/test/java/io/priyank/wdcd/it/ControllerIntegrationTest.java
@@ -6,13 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultActions;
-
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
